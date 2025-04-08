a4093ff860096245ae98eb4e9c7df8f6f07ed8b2
mbh95 <matthewbh95@gmail.com>
2025-04-08 09:10:28 -0400

    Create LibretroBridge module, fix project settings.

---
+++ SwiftRetro/LibretroBridge/LibretroBridge.h
@@ -0,0 +1,14 @@
+//
+//  LibretroBridge.h
+//  SwiftRetro
+//
+//  Created by Matt Hammond on 4/8/25.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef LibretroBridge_h
+#define LibretroBridge_h
+
+
+#endif /* LibretroBridge_h */
---
+++ SwiftRetro/LibretroBridge/LibretroBridge.m
@@ -0,0 +1,9 @@
+//
+//  LibretroBridge.m
+//  SwiftRetro
+//
+//  Created by Matt Hammond on 4/8/25.
+//
+
+#import <Foundation/Foundation.h>
+#include "libretro.h"
