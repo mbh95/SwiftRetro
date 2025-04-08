c075203fd2f74c4ef24be6e584af7f7926198839
mbh95 <matthewbh95@gmail.com>
2025-04-08 08:47:28 -0400

    Initial Commit

---
+++ SwiftRetro/SwiftRetroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRetroApp.swift
+//  SwiftRetro
+//
+//  Created by Matt Hammond on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRetroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
